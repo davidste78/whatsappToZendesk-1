dc79ead5b5f2920e034f8ce3a8c1044ff11fde02
Guillermo Maquieira <guillermom@olx.com>
2015-05-29 14:42:05 -0300

    WZ :: Set Profile Picture

--- app/container.php
+++ app/container.php
@@ -6,9 +6,14 @@ $c['whatsapp.client'] = function() {
     return new \WaToZendApp\Command\WaToZendCommand();
 };
 
+$c['whatsapp.setprofilepicture'] = function() {
+    return new \WaToZendApp\Command\SetProfilePictureCommand();
+};
+
 $c['commands'] = function($c) {
     return array(
-        $c['whatsapp.client']
+        $c['whatsapp.client'],
+        $c['whatsapp.setprofilepicture']
     );
 };
 
